053f117d90a6540286d81054988eb0a6e033e5c5
mangosax <mcguerty@att.net>
2023-09-20 07:18:46 -0500

    first push

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.Design;
+using System.Runtime.InteropServices;
+
+namespace FizzBuzz
+{
+    internal class Program
+    {
+
+        public static string FBChallenge(int tmpNbr)
+        {
+            if (tmpNbr %3 ==0  &&  tmpNbr %5 ==0) 
+            {
+                return "fizzbuzz";
+            }
+            else if (tmpNbr %3 == 0) 
+            {
+                return "fizz";
+            }
+            else if (tmpNbr %5 == 0) 
+            {
+                return "buzz";
+            }
+            else
+            {
+                return "nobuzz";
+            }
+        }
+
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Please enter an integer> ");
+            int tmpNum = int.Parse((Console.ReadLine()));
+            Console.WriteLine(FBChallenge(tmpNum));
+
+
+        }
+    }
+}
